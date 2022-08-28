7610a83fa19abd5a7e24bd8d1481d39ce5e8cf5b
Kamil Choiński <110kc3@gmail.com>
2022-08-29 01:27:09 +0200

    Changing to cloud SQL on GCP

--- README.md
+++ README.md
@@ -8,3 +8,13 @@ conda activate flask-test-app
 In VS Code, open the Command Palette (View > Command Palette or (Ctrl+Shift+P)). Then select the Python: Select Interpreter command:
 
 pip install -r requirements.txt
+
+python run.py
+
+
+
+
+
+
+Database: https://console.cloud.google.com/sql/instances/mysql-5-7-instance/connections/networking?authuser=7&cloudshell=true&project=sonic-column-358417
+
--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,27 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
-
+from flask_cors import CORS
+from dotenv import load_dotenv
+load_dotenv()
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+
+
+
+
 app.config['SECRET_KEY'] = 'super secret key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
+app.config["SQLALCHEMY_DATABASE_URI"]= os.environ.get("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= True
+ 
+
+
+
+
+CORS(app)
+
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'mydatabase.db')
 db = SQLAlchemy(app)
 
